33a07f318006f3a901be95a5ebebd9640febb379
Alexander Loport <sasha-loport1488@yandex.ru>
2019-10-16 00:54:59 +0300

    сделал интерфейс

--- MagicSquare/MagicSquareGenerator.cs
+++ MagicSquare/MagicSquareGenerator.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MagicSquare
 {
-    class MagicSquareGenerator
+    static class MagicSquareGenerator
     {
+        public static List<List<int>> Generate(int size)
+        {
+            List<List<int>> result = new List<List<int>>();
 
+            for (int i = 0; i < size; i++)
+            {
+                result.Add(new List<int>());
+                for (int j = 0; j < size; j++)
+                {
+                    result[i].Add(0);
+                }
+            }
+
+            return result;
+        }
     }
 }
--- MagicSquare/MainWindow.xaml.cs
+++ MagicSquare/MainWindow.xaml.cs
@@ -27,7 +27,44 @@ namespace MagicSquare
 
         private void btnGenerate_Click(object sender, RoutedEventArgs e)
         {
+            if (tbxSize.Text != "")
+            {
+                GenerateGrid(MagicSquareGenerator.Generate(Convert.ToInt32(tbxSize.Text)));
+            }
+        }
 
+        private void GenerateGrid(List<List<int>> matrix)
+        {
+            Grid magicGrid = new Grid();
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                magicGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                magicGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                for (int j = 0; j < matrix.Count; j++)
+                {
+                    TextBlock tb = new TextBlock
+                    {
+                        Text = matrix[i][j].ToString(),
+                        FontSize = 18,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center
+                    };
+
+                    Grid.SetRow(tb, i);
+                    Grid.SetColumn(tb, j);
+                    magicGrid.Children.Add(tb);
+                }
+            }
+
+            magicGrid.ShowGridLines = true;
+            
+            grid.Children.Clear();
+            grid.Children.Add(magicGrid);
         }
     }
 }
